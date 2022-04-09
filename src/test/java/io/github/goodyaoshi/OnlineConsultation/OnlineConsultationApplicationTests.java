92ead3b9c2c907effe75fa26ea88f02218af095e
Goodyaoshi <liyaoshi0328@qq.com>
2022-04-09 19:51:37 +0800

    Initial commit

---
+++ src/test/java/io/github/goodyaoshi/OnlineConsultation/OnlineConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.goodyaoshi.OnlineConsultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
